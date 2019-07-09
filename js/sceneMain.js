7161eb5433cf7e38fce95b059520a634339e6fff
unknown <n_stefanov@hotmail.com>
2019-07-09 14:38:00 +0300

    added basic files and folders structure

---
+++ js/sceneMain.js
@@ -0,0 +1,17 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+
+    preload() {
+        //  load resources
+    }
+
+    create() {
+        // create objects
+    }
+
+    update() {
+        // constant running loop
+    }
+}
